034b0d0283e07b7884e7f3e6b18b4b321079d8dd
Dhangni <dhanikusuma256@gmail.com>
2025-12-15 23:33:42 +0700

    logout langsung ke /

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -72,6 +72,6 @@ class Auth extends BaseController
     public function logout()
     {
         session()->destroy();
-        return redirect()->to('/login');
+        return redirect()->to('/');
     }
 }
